3ee507c11587497c47129ac4a81f433b9d0578fb
YamaguchiKohei <YamaguchiKohei@localhost>
2021-09-11 23:28:33 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -49,7 +49,7 @@ public class CreateServlet extends HttpServlet {
             m.setUpdated_at(currentTime);
 
             em.persist(m);
-            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
